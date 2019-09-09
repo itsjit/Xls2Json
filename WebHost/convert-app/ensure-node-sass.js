effe0a8e07907adc5e61bd11daec3ec7adf05a93
itsjit <itsthejit@gmail.com>
2019-09-09 11:33:48 +0200

    Check if sass rebuilt is needed.

---
+++ WebHost/convert-app/ensure-node-sass.js
@@ -0,0 +1,29 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+function verify() {
+  const os = process.platform.toLowerCase();
+  console.log(`[verify-sass] Your operating system: ${os}`);
+
+  return fs.readdirSync('./node_modules/node-sass/vendor').filter(fn => fn.startsWith(os)).length ? true : false;
+}
+
+function rebuild() {
+  return new Promise((resolve, reject) => {
+    exec('npm rebuild node-sass --force').on('exit', resolve);
+  });
+}
+
+console.log('[verify-sass] Verifying correct node-sass binary exists.');
+var needRebuild = verify();
+
+if (needRebuild) {
+  console.log('[verify-sass] Verified!');
+} else {
+  console.log('[verify-sass] Binary missing for this OS!');
+  console.log("[verify-sass] Running 'npm rebuild node-sass --force'.");
+  console.log('[verify-sass] This may take a moment...');
+  rebuild().then(() => {
+    console.log('[verify-sass] Finished!');
+  });
+}
