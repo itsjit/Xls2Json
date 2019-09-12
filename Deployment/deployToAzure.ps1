7c5afe072c3801e87cc09af27d1d321a841f9ccc
itsjit <itsthejit@gmail.com>
2019-09-12 10:59:58 +0200

    Use secrets.json

--- Deployment/deployToAzure.ps1
+++ Deployment/deployToAzure.ps1
@@ -1,31 +1,28 @@
 Import-Module $PSScriptRoot\scripts\AzureAccount.psm1 -Force -ErrorAction Stop
 Import-Module $PSScriptRoot\scripts\Publish-WebApp.psm1 -Force -ErrorAction Stop
-Import-Module $PSScriptRoot\Env.psm1 -Force -ErrorAction Stop
 
-if (-not $DeploymentEnvironment) {
-    throw "You must provide environment configuration in 'DeploymentEnvironment' global variable."
+$secrets = Get-Content "$PSScriptRoot\secrets.json" | ConvertFrom-Json
+if (-not $secrets) {
+    throw "You must provide environment configuration in 'secrets.json' file."
 }
 if (-not (Get-InstalledModule -Name Az -AllVersions)) {
     Write-Host "Module 'AZ' has not been found. To install the module, please run: 'Install-Module -Name Az -AllowClobber'."
 } 
 
-if ($null -ne $DeploymentEnvironment.ApplicationSecret) {
+if (-not [String]::IsNullOrEmpty($secrets.ApplicationSecret)) {
     $applicationSecret = ConvertTo-SecureString `
-        $DeploymentEnvironment.ApplicationSecret `
+        $secrets.ApplicationSecret `
         -AsPlainText `
         -Force
 }
 
 Submit-LogIn `
-    -SubscriptionId $DeploymentEnvironment.SubscriptionId `
-    -TenantId $DeploymentEnvironment.TenantId `
-    -ApplicationId $DeploymentEnvironment.ApplicationId `
+    -SubscriptionId $secrets.SubscriptionId `
+    -TenantId $secrets.TenantId `
+    -ApplicationId $secrets.ApplicationId `
     -ApplicationSecret $applicationSecret
-
-$ResourceGroupName = $DeploymentEnvironment.WebApp.ResourceGroupName
-$WebAppName = $DeploymentEnvironment.WebApp.WebAppName
 
 Publish-WebApp `
     -Project           :\WebHost `
-    -Name              $WebAppName `
-    -ResourceGroupName $ResourceGroupName 
+    -Name              $secrets.WebApp.WebAppName `
+    -ResourceGroupName $secrets.WebApp.ResourceGroupName 
